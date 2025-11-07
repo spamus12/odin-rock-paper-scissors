1ecc12b522eeeba6bf1b8432ad27386b667d63db
spamus12 <gwd068@gmail.com>
2025-11-07 13:30:53 -0600

    Implement computerChoice() function
    computerChoice() returns a random choice between rock, paper, scissors

---
+++ rock-paper-scissors.js
@@ -0,0 +1,6 @@
+// rock-paper-scissors.js
+const choices = ["rock", "paper", "scissors"];
+
+
+// Choose a random integer from 0 to 2 and return the choice from the array
+let computerChoice = () => { return choices[ Math.floor( Math.random() * 3 ) ] };

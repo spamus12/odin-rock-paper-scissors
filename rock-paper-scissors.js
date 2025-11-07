a815749326abff98af5100617f5ce931e10feffd
spamus12 <gwd068@gmail.com>
2025-11-07 13:37:56 -0600

    Declare getHumanChoice() and score variables

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,8 +1,21 @@
 // rock-paper-scissors.js
+
+
+// Array of choices
 const choices = ["rock", "paper", "scissors"];
 
+// Scoring
+let computerScore = 0;
+let humanScore = 0;
 
 // Choose a random integer from 0 to 2 and return the choice from the array
 let getComputerChoice = () => { return choices[ Math.floor( Math.random() * 3 ) ] };
 
-// 
+// Get the player to choose an option
+// The choices are case-insensitive
+let getHumanChoice = () => {
+    let choice = prompt("Rock, paper, or scissors?");
+    return choice.toLowerCase();
+};
+
+console.log(getHumanChoice());

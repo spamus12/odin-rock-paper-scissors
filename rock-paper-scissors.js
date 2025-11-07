cd0f2fd6a60c4d4be3bbc8ae64f810388a944df2
spamus12 <gwd068@gmail.com>
2025-11-07 13:32:01 -0600

    Rename computerChoice() to getComputerChoice()

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,4 +3,6 @@ const choices = ["rock", "paper", "scissors"];
 
 
 // Choose a random integer from 0 to 2 and return the choice from the array
-let computerChoice = () => { return choices[ Math.floor( Math.random() * 3 ) ] };
+let getComputerChoice = () => { return choices[ Math.floor( Math.random() * 3 ) ] };
+
+// 
